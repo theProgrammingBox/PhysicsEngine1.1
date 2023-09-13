81487987a8a9bec0d192d410e52f7a70679eb920
theProgrammingBox <justinboxgea@gmail.com>
2023-09-12 17:20:24 -0700

    working

--- PhysicsEngine1.1/Source.cpp
+++ PhysicsEngine1.1/Source.cpp
@@ -19,8 +19,20 @@ struct Ball
 	float invElasticity;
 	olc::Pixel color;
 
-	Ball(olc::vf2d _pos, olc::vf2d _vel, olc::vf2d _acc, float _radius, float _density, float _elasticity, olc::Pixel _color)
-		: pos(_pos), vel(_vel), acc(_acc), radius(_radius), density(_density), invMass(1.0f / (_density * 3.14159f * _radius * _radius)), invElasticity(1.0f / _elasticity), color(_color)
+	Ball(olc::vf2d _pos, olc::vf2d _vel, float _radius, float _density = 1.0f, float _elasticity = 1.0f, olc::Pixel _color = olc::WHITE)
+		: pos(_pos), vel(_vel), acc(olc::vf2d(0, 0)), radius(_radius), density(_density), invMass(1.0f / (_density * 3.14159f * _radius * _radius)), invElasticity(1.0f / _elasticity), color(_color)
+	{}
+};
+
+struct Wall
+{
+	olc::vf2d pos1;
+	olc::vf2d pos2;
+	olc::vf2d normal;
+	olc::Pixel color;
+
+	Wall(olc::vf2d _pos1, olc::vf2d _pos2, olc::Pixel _color = olc::WHITE)
+		: pos1(_pos1), pos2(_pos2), normal( pos2 - pos1 ), color(_color)
 	{}
 };
 
@@ -33,19 +45,23 @@ class Example : public olc::PixelGameEngine
 {
 public:
 	std::vector<Ball> balls;
+	std::vector<Wall> walls;
 
 	bool OnUserCreate() override
 	{
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < 16; i++)
 			balls.push_back(Ball(
 				{ FloatRand(0, ScreenWidth()), FloatRand(0, ScreenHeight()) },
 				{ FloatRand(-1, 1), FloatRand(-1, 1) },
-				{ 0, 0 },
-				FloatRand(2, 16),
-				1.0f,
-				1.0f,
-				olc::Pixel(rand() % 255, rand() % 255, rand() % 255)
+				FloatRand(2, 16)
 			));
+
+		for (int i = 0; i < 4; i++)
+			walls.push_back(Wall(
+				{ FloatRand(0, ScreenWidth()), FloatRand(0, ScreenHeight()) },
+				{ FloatRand(0, ScreenWidth()), FloatRand(0, ScreenHeight()) }
+			));
+
 		return true;
 	}
 
@@ -58,6 +74,9 @@ public:
 			DrawLine(ball.pos, ball.pos + ball.vel * 10, olc::BLACK);
 			DrawLine(ball.pos, ball.pos + ball.acc * 100, olc::BLACK);
 		}
+
+		for (auto& wall : walls)
+			DrawLine(wall.pos1, wall.pos2, olc::BLACK);
 	}
 
 	void Controls()
@@ -73,12 +92,12 @@ public:
 			acc.x += 1;
 		float mag2 = acc.mag2();
 		if (mag2 > 0)
-			balls.front().acc = acc * 0.02f * InvSqrt(mag2);
+			balls.front().acc = acc * 0.002f * InvSqrt(mag2);
 	}
 
 	void Collision()
 	{
-		for (int i = 0; i < balls.size(); i++)
+		for (int i = 0; i < balls.size() - 1; i++)
 		{
 			for (int j = i + 1; j < balls.size(); j++)
 			{
@@ -87,16 +106,40 @@ public:
 				float radii = balls[j].radius + balls[i].radius;
 				if (dist2 < radii * radii && dist2 > 0.0001f)
 				{
-					olc::vf2d normal = delta * InvSqrt(dist2);
+					olc::vf2d normal = delta / sqrt(dist2);
 					olc::vf2d vecc = balls[i].vel - balls[j].vel;
 					float elasticity = 2.0f / (balls[i].invElasticity + balls[j].invElasticity);
-					float mass = 1.0f / (balls[i].invMass + balls[j].invMass);
+					float mass = 2.0f / (balls[i].invMass + balls[j].invMass);
 					float dot = vecc.dot(normal) * mass * elasticity;
 					if (dot > 0)
 					{
 						balls[i].vel -= normal * dot * balls[i].invMass;
 						balls[j].vel += normal * dot * balls[j].invMass;
 					}
+				}
+			}
+		}
+
+		for (auto& ball : balls)
+		{
+			for (auto& wall : walls)
+			{
+				olc::vf2d wallDelta = wall.pos2 - wall.pos1;
+				float wallLength = wallDelta.mag();
+				olc::vf2d wallNormal = wallDelta / wallLength;
+				olc::vf2d ballDelta = ball.pos - wall.pos1;
+				float dot = ballDelta.dot(wallNormal);
+				dot = std::max(0.0f, std::min(dot, wallLength));
+				olc::vf2d closestPoint = wall.pos1 + wallNormal * dot;
+
+				float dist2 = (ball.pos - closestPoint).mag2();
+				if (dist2 < ball.radius * ball.radius && dist2 > 0.0001f)
+				{
+					olc::vf2d ballNormal = (ball.pos - closestPoint) / sqrt(dist2);
+					float elasticity = 2.0f / (ball.invElasticity + 1.0f);
+					float dot = ball.vel.dot(ballNormal) * elasticity * 2.0f;
+					if (dot < 0)
+						ball.vel -= ballNormal * dot;
 				}
 			}
 		}
@@ -148,6 +191,9 @@ public:
 			DrawLine(ball.pos, ball.pos + ball.vel * 10, olc::RED);
 			DrawLine(ball.pos, ball.pos + ball.acc * 100, olc::GREEN);
 		}
+
+		for (auto& wall : walls)
+			DrawLine(wall.pos1, wall.pos2, wall.color);
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
@@ -155,7 +201,7 @@ public:
 		Unrender();
 		Controls();
 		Collision();
-		Update(1);
+		Update(fElapsedTime * 100);
 		Render();
 
 		return true;
